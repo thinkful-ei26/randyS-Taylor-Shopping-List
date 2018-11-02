a6231d6235201b3b757574a0e25a10e0d04f5ac6
taylormerrill <taylormerrill92@gmail.com>
2018-11-01 21:56:13 -0600

    finished the checklist function to work with added items and original items

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function deleteTarget(){
         }); 
 }
 function checkTarget(){
-    $('li').on('click', function(event){
-          
-        $(this).closest('li').children().first().toggleClass('shopping-item__checked');
-        }); 
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+        debugger;
+      });
 }
 
 
